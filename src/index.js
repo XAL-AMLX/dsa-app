d0d727f1c5c6579f7a3d5cd42563e27ee0d4ae4b
XAL-AMLX <amaljiths612@gmail.com>
2022-10-18 09:39:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+import dsaquestions from "./Sheet1.json"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App dsaquestions={dsaquestions} />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
