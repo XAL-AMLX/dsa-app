c8b46f6522a3689e7a61a93ea0db55b7f1c06d87
XAL-AMLX <amaljiths612@gmail.com>
2022-10-18 09:38:57 +0530

    Create SQ.jsx

---
+++ src/components/questions/SQ.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { QuestionsContext } from '../../App';
+import QueRequest from './QueRequest';
+
+const SQ = () => {
+  return (
+    <div>
+      
+        <QuestionsContext.Consumer>
+          
+            {(value) =>
+              value?.map((val) => {
+                return <QueRequest question={val} />;
+              })
+            }
+          
+        </QuestionsContext.Consumer>
+      
+    </div>
+  );
+}
+
+export default SQ
